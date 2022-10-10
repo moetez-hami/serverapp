b420899175154fd51664207b9159d38d5e73e3c0
moetez-hami <65268618+moetez-hami@users.noreply.github.com>
2022-10-10 22:29:37 +0100

    Server Service Part 2

--- src/app/interface/custom-response.ts
+++ src/app/interface/custom-response.ts
@@ -1,10 +1,12 @@
-import { Server } from "http";
+import { Server } from "./server";
+
 
 export interface CustomResponse {
     timeStamp: Date;
     statusCode: string;
     status:string;
     reason:string;
+    message: string;
     developperMessage: string;
     data:{servers?: Server[], server?: Server };
 }
